5b45c1c87fd60fedf906f24a16b1bc4b237c70e9
Duvan Ayala Stonic <ayalastonic@Duvans-MBP.mshome.net>
2024-02-28 17:14:07 -0500

    adding more pictures and colors/styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { CatsComponent } from './cats/cats.component';
+import { DogsComponent } from './dogs/dogs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    CatsComponent,
+    DogsComponent
   ],
   imports: [
     BrowserModule,
